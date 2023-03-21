2b253706be814b61efa2eee780f13e1954ecd1dd
Jacob Hummer <jcbhmr@outlook.com>
2023-03-20 20:01:31 -0500

    Use INPUT_ instead of INPUTS_
    INPUT_ = 592 results https://github.com/search?q=path%3A%2Faction.yml+INPUT_&type=code
    vs
    INPUTS_ = 13 results https://github.com/search?q=path%3A%2Faction.yml+INPUTS_&type=code

--- src/index.sh
+++ src/index.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-cd "$INPUTS_WIKI_DIRECTORY"
+cd "$INPUT_WIKI_DIRECTORY"
 
 git init
 git config --local user.name github-actions
 git config --local user.email github-actions@github.com
-git remote add origin https://$INPUTS_TOKEN@$INPUTS_REPOSITORY.wiki.git
+git remote add origin https://$INPUT_TOKEN@$INPUT_REPOSITORY.wiki.git
 git fetch origin
 git reset origin/master
 
 git add --all
-git commit -m "$INPUTS_COMMIT_MESSAGE"
+git commit -m "$INPUT_COMMIT_MESSAGE"
 git push -u origin master
 
 rm -rf ./.git
