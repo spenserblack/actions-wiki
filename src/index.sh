a5b0b949fec5746d2e9a6e12c2b2da49fb73bb2d
Jacob Hummer <jcbhmr@outlook.com>
2023-03-22 00:38:23 +0000

    Fix INPUTS_ => INPUT_ from merge issues

--- src/index.sh
+++ src/index.sh
@@ -3,8 +3,8 @@ set -e
 
 source "$(dirname "${BASH_SOURCE[0]}")/git_exists.sh"
 
-if ! git_exists "https://$INPUTS_TOKEN@$INPUTS_REPOSITORY.wiki.git" &>/dev/null; then
-  echo "$(basename "$0"): https://$INPUTS_REPOSITORY.wiki.git doesn't exist." >&2
+if ! git_exists "https://$INPUT_TOKEN@$INPUT_REPOSITORY.wiki.git" &>/dev/null; then
+  echo "$(basename "$0"): https://$INPUT_REPOSITORY.wiki.git doesn't exist." >&2
   echo 'Did you remember to create the first wiki page manually?' >&2
   echo 'You can find more information on the readme. [1]' >&2
   echo
@@ -12,12 +12,12 @@ if ! git_exists "https://$INPUTS_TOKEN@$INPUTS_REPOSITORY.wiki.git" &>/dev/null;
   exit 1
 fi
 
-cd "$INPUTS_WIKI_DIRECTORY"
+cd "$INPUT_WIKI_DIRECTORY"
 
 git init
 git config --local user.name github-actions
 git config --local user.email github-actions@github.com
-git remote add origin https://$INPUTS_TOKEN@$INPUTS_REPOSITORY.wiki.git
+git remote add origin https://$INPUT_TOKEN@$INPUT_REPOSITORY.wiki.git
 git fetch origin
 git reset origin/master
 
