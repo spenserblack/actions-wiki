58dba7358e792d8302fb1edb882b14ae4722c145
Jacob Hummer <jcbhmr@outlook.com>
2023-05-02 13:45:56 -0500

    Use $GITHUB_ env vars more

--- index.sh
+++ index.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 set -e
 
-# The GitHub CLI (gh) uses $GH_TOKEN. You don't need to do gh auth login with a
-# real user if you already have a token.
-export GH_TOKEN="$INPUT_TOKEN"
-gh auth setup-git
+# Override the github.* context env vars with the ones that we got from the user
+# that may (or may not be) different from the original $GITHUB_* env vars.
+export GITHUB_TOKEN="$INPUT_TOKEN"
+export GITHUB_SERVER_URL="$INPUT_GITHUB_SERVER_URL"
+export GITHUB_REPOSITORY="$INPUT_REPOSITORY"
 
-repository_url="$INPUT_GITHUB_SERVER_URL/$INPUT_REPOSITORY.wiki"
+# This is the default host that gh uses for clones and commands without a repo
+# context (a .git folder). Assuming $GITHUB_SERVER_URL is something like
+# "https://example.org" WITHOUT a trailing slash, then this will basically do:
+# github.server_url.replace(/^.*\//, "") to remove the "https://" part, leaving
+# just the host "example.org".
+export GH_HOST="${GITHUB_SERVER_URL#*/}"
+
+gh auth setup-git
 
 cd "$INPUT_PATH"
 
-if ! gh repo clone "$repository_url" .git -- --bare; then
-  echo "$(basename "$0"): $repository_url doesn't exist." >&2
+if ! gh repo clone "$GITHUB_REPOSITORY.wiki" .git -- --bare; then
+  echo "$(basename "$0"): $GITHUB_REPOSITORY.wiki doesn't exist." >&2
   echo 'Did you remember to create the first wiki page manually?' >&2
   echo 'You can find more information on the readme.'
   echo 'https://github.com/spenserblack/actions-wiki#usage' >&2
+  echo >&2
+  echo "\$GITHUB_SERVER_URL=$GITHUB_SERVER_URL" >&2
+  echo "\$GH_HOST=$GH_HOST" >&2
+  echo "\$GITHUB_REPOSITORY=$GITHUB_REPOSITORY" >&2
   exit 1
 fi
 # https://stackoverflow.com/a/28180781
@@ -22,8 +34,8 @@ git config --unset core.bare
 git reset
 
 # https://github.com/stefanzweifel/git-auto-commit-action/blob/9cc0a1f55de2aa04a36721ca84463dd520f70f4c/action.yml#L35-L42
-git config --local user.name github-actions[bot]
-git config --local user.email 41898282+github-actions[bot]@users.noreply.github.com
+git config user.name github-actions[bot]
+git config user.email 41898282+github-actions[bot]@users.noreply.github.com
 
 # https://stackoverflow.com/a/2659808
 if git diff-index --quiet HEAD --; then
