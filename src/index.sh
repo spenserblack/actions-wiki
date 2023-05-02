1c0a32a6eaaa020b88fcad8c5589ed1f891ba7f5
Jacob Hummer <jcbhmr@outlook.com>
2023-05-02 13:32:22 -0500

    Use gh cli and --bare hack

--- src/index.sh
+++ src/index.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 set -e
 
-# shellcheck source=src/git_exists.sh
-source "$(dirname "${BASH_SOURCE[0]}")/git_exists.sh"
+# The GitHub CLI (gh) uses $GH_TOKEN. You don't need to do gh auth login with a
+# real user if you already have a token.
+export GH_TOKEN="$INPUT_TOKEN"
+gh auth setup-git
 
-if ! git_exists "https://$GITHUB_ACTOR:$INPUT_TOKEN@$INPUT_REPOSITORY.wiki.git" &>/dev/null; then
-  echo "$(basename "$0"): https://$INPUT_REPOSITORY.wiki.git doesn't exist." >&2
-  echo 'Did you remember to create the first wiki page manually?' >&2
-  echo 'You can find more information on the readme. [1]' >&2
-  echo
-  echo '[1]: https://github.com/spenserblack/actions-wiki#usage'
-  exit 1
-fi
+repository_url="$INPUT_GITHUB_SERVER_URL/$INPUT_REPOSITORY.wiki"
 
 cd "$INPUT_PATH"
 
-git init
-git config --local user.name github-actions
-git config --local user.email github-actions@github.com
-git remote add origin "https://$GITHUB_ACTOR:$INPUT_TOKEN@$INPUT_REPOSITORY.wiki.git"
-git fetch origin
-git reset origin/master
+if ! gh repo clone "$repository_url" .git -- --bare; then
+  echo "$(basename "$0"): $repository_url doesn't exist." >&2
+  echo 'Did you remember to create the first wiki page manually?' >&2
+  echo 'You can find more information on the readme.'
+  echo 'https://github.com/spenserblack/actions-wiki#usage' >&2
+  exit 1
+fi
+# https://stackoverflow.com/a/28180781
+git config --unset core.bare
+git reset
+
+# https://github.com/stefanzweifel/git-auto-commit-action/blob/9cc0a1f55de2aa04a36721ca84463dd520f70f4c/action.yml#L35-L42
+git config --local user.name github-actions[bot]
+git config --local user.email 41898282+github-actions[bot]@users.noreply.github.com
 
 # https://stackoverflow.com/a/2659808
 if git diff-index --quiet HEAD --; then
 	echo 'No changes! Will not commit or push.'
-	rm -rf .git
 	exit 0
 fi
 
-git add --all
+git add -A
 git commit -m "$INPUT_COMMIT_MESSAGE"
 
 if [[ $INPUT_DRY_RUN == true ]]; then
@@ -38,5 +40,3 @@ if [[ $INPUT_DRY_RUN == true ]]; then
 else
 	git push -u origin master
 fi
-
-rm -rf ./.git
